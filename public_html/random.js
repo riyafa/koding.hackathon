580d39ff8f9f9ae7e209ca50d8b8729da0d99ffc
riyafa <riyafa0@gmail.com>
2014-12-07 11:49:56 +0530

    The lines are selected properly

--- public_html/random.js
+++ public_html/random.js
@@ -1,6 +1,62 @@
+function draw(n,rep){
+    var Edge = function(source, sink, capacity) {
+        this.source = source;
+        this.sink = sink;
+        this.capacity = capacity;
+    };
 
+// Main class to manage the network
+    var Graph = function() {
+        this.edges = {};
+        this.nodes = [];
+        this.nodeMap = {};
+        // Add a node to the graph
+        this.addNode = function(node) {
+        this.nodes.push(node);
+        this.nodeMap[node] = this.nodes.length-1;
+        this.edges[node] = [];
+    };
     
-    function draw(n,rep){
+    // Add an edge from source to sink with capacity
+    this.addEdge = function(source, sink, capacity) {
+        // Create the two edges = one being the reverse of the other
+        this.edges[source].push(new Edge(source, sink, capacity));
+        this.edges[sink].push(new Edge(sink, source, capacity));
+    };
+    // Does edge from source to sink exist?
+    this.edgeExists = function(source, sink) {
+        if(this.edges[source] !== undefined)
+            for(var i=0;i<this.edges[source].length;i++)
+                if(this.edges[source][i].sink === sink)
+                    return this.edges[source][i];
+                    return null;
+                };
+    };
+
+    function Prim(graph) {
+        var result = [];
+        var usedNodes = {};
+        function findMin(g) {
+            var min = [999999,null];
+            for(var i=0;i<result.length;i++)
+                for(var n=0;n<g.edges[result[i]].length;n++)
+                    if(g.edges[result[i]][n].capacity < min[0] && usedNodes[g.edges[result[i]][n].sink] === undefined)
+                        min = [g.edges[result[i]][n].capacity, g.edges[result[i]][n].sink];
+            return min[1];
+        }
+        // Pick random start point
+        var node = g.nodes[Math.round(Math.random()*(g.nodes.length-1))];
+        result.push(node);
+        usedNodes[node] = true;
+        var min = findMin(g);
+        while(min !==null) {
+            result.push(min);
+            usedNodes[min] = true;
+            min = findMin(g);
+        }
+        return result;
+    };
+
         var cvs=document.getElementsByTagName('canvas')[0];   
             /** 
              * @type CanvasRenderingContext2D 
@@ -27,15 +83,16 @@
 		var cheight = cvs.height;                
                 randomX = Math.random()*cwidth*2/3;
 		randomY = Math.random()*cheight*2/3;
+                		
+                ctx.fillRect(randomX,randomY,5,5);		
+		ctx.moveTo(lastRandomx,lastRandomy);
+		ctx.lineTo(randomX,randomY); 
                 weights.push(Math.round(Math.random()*20)); 
                 var p1={x:lastRandomx,y:lastRandomy};
                 var p2={x:randomX,y:randomY};
                //var p1 = {x:200,y:200};
                 //var p2 = {x:400,y:300};	
-                drawLabel(ctx,weights[i].toString(),p1,p2,'center',10);			
-                ctx.fillRect(randomX,randomY,5,5);		
-		ctx.moveTo(lastRandomx,lastRandomy);
-		ctx.lineTo(randomX,randomY);               
+                drawLabel(ctx,weights[i].toString(),p1,p2,'center',10);	
                 lastRandomx=randomX;
                 lastRandomy=randomY;
                 lines.push({              // store point to create a poly-line
@@ -52,11 +109,12 @@
                     y: lines[rand].y
                   });
                   weights.push(Math.round(Math.random()*20)); 
-                    p1={x:lastRandomx,y:lastRandomy};
-                    p2={x:randomX,y:randomY};               	
-                drawLabel(ctx,weights[n+i].toString(),p1,p2,'center',10);	
-            } 
-            ctx.closePath();
+                p2={x:lines[rand].x,y:lines[rand].y};
+                p1={x:lastRandomx,y:lastRandomy};               	
+                drawLabel(ctx,weights[n+i].toString(),p1,p2,'center',10);
+                lastRandomx=lines[rand].x;
+                lastRandomy=lines[rand].y;
+            }             
             ctx.stroke();
             
              cvs.addEventListener('click',
